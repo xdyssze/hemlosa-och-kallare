cf64bb33fc00b0c2872380aa5048bb3e3262ec8b
xdyssze <53408336+xdyssze@users.noreply.github.com>
2022-09-22 13:06:42 +0200

    Sprites Update
    continued Progress on sprites

--- src/gfxproc/Gfx.java
+++ src/gfxproc/Gfx.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Element;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -21,83 +20,18 @@ public class Gfx {
 		    case ("set"): {
 		        sizex = Integer.parseInt(args[1]);
 		        sizey = Integer.parseInt(args[2]);
-		        pix = new Segment[sizex*sizey];
-		        plays = new int[12];
-		        
-		     // SPRITES:
-				try {
-					File xml = new File(System.getProperty("user.dir") + "\\resources\\sprites.xml");	
-					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-					DocumentBuilder builder = factory.newDocumentBuilder();
-					doc = builder.parse(xml);
-					
-					} catch(Exception e) {
-						e.printStackTrace();			
-					}
-				// ORdern är spelare, föremål, karaktärer
-				System.out.print(doc.getElementsByTagName("sprite").getLength());
-				sprites = new Sprite[doc.getElementsByTagName("sprite").getLength()];
-				objs = new int[doc.getElementsByTagName("sprite").getLength()-12];
-				chars = new int[doc.getElementsByTagName("sprite").getLength()-12];
-				
-				for(int i = 0; i < doc.getElementsByTagName("sprite").getLength(); i++) {
-					Node s = doc.getElementsByTagName("sprite").item(i);
-				    NodeList d = s.getChildNodes();
-				    switch(d.item(1).getTextContent()) {
-				    case("player"): {
-				    	addToEnd(plays, i, null, null);
-				    	sprites[i] = new Sprite(i, d);
-				    	break;
-				    }
-                    case("object"): {
-                    	addToEnd(objs, i, null, null);
-                    	sprites[i] = new Sprite(i, d);
-				    	break;
-				    }
-                    case("character"): {
-                    	addToEnd(chars, i, null, null);
-                    	sprites[i] = new Sprite(i, d);
-				    	break;
-				    }
-				    
-				    }
-				}
-			
-				// END OF SPRITES
-		        
+		        pix = new Segment[sizex*sizey];		        
+		        SpriteHandler.main();
 		        Seghand("cr", 0, 0, ' ');
                 break;
 		    }
-		    default: {
-		    	
+		    default: {		    	
 		    	break;
-		    }
-		
-	    }
-		
-		}
-		
+		    }		
+	    }		
+		}		
 	}
-	public static void addToEnd(int[] a, int v, String[] as, String vs) {
-	   if(a == null) {
-		   for(int i = 0; i < as.length; i++) {
-			   if(as[i] != null) {
-				   as[i] = vs;
-				   i = as.length;
-			   }
-			   
-		   }
-	   } else {
-		   for(int i = 0; i < a.length; i++) {
-			   if(a[i] == 0) {
-				   a[i] = v;
-				   i = a.length;
-			   }
-			   
-		   }
-	   }
-		
-	}
+	// RENDERING
 	public static String rends() {
 		String s = "";
 		for(int i = 0; i < sizey; i++) {
@@ -108,31 +42,24 @@ public class Gfx {
 		    s += "\r\n";
 		}
 		return(s);
-	}
+	}	
 	
-	public static String SpriteHand() {
-		
-		return("");
-	}
+	// SEGMENTS
 	public static void Seghand(String ar1, int ar2, int ar3, char ar4) {
 		switch (ar1) {
 		    case ("cr"): {
 		    	for(int i = 0; i < sizex*sizey; i++) {
-		    		try {
-		    			
-		    		pix[i] = new Segment(i);
-		    		
+		    		try {		    			
+		    		pix[i] = new Segment(i);		    		
 		    		} catch(Exception e) {
 		    			System.out.println(e);
 		    		}
 		    	}		    		
 		    	break;
 		    }
-		    case ("chng"): {
-		    
+		    case ("chng"): {		    
 		    	int id = sizex*(ar3)+ar2;
-		    	pix[id].symbol = ar4;
-		    	
+		    	pix[id].symbol = ar4;		    	
 		    	break;
 		    }
 		    case ("clr"): {
@@ -146,15 +73,7 @@ public class Gfx {
 			}   	
 		}		
 	}
-	public static void CLS(){
-
-	    
-	    try {
-	        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	    } catch (IOException | InterruptedException ex) {}
-
-	    }
-
+	
 	public static class Segment {
 		public char symbol;		
 		int id;
@@ -163,6 +82,76 @@ public class Gfx {
 			this.symbol = ' ';
 		}				
 		}
+	
+	// SPRITES
+	
+	public static class SpriteHandler {
+		public static void main() {
+			  plays = new int[12];		        
+			     
+					try {
+						File xml = new File(System.getProperty("user.dir") + "\\resources\\sprites.xml");	
+						DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+						DocumentBuilder builder = factory.newDocumentBuilder();
+						doc = builder.parse(xml);
+						
+						} catch(Exception e) {
+							e.printStackTrace();			
+						}
+					System.out.print(doc.getElementsByTagName("sprite").getLength());
+					sprites = new Sprite[doc.getElementsByTagName("sprite").getLength()];
+					objs = new int[doc.getElementsByTagName("sprite").getLength()];
+					chars = new int[doc.getElementsByTagName("sprite").getLength()];
+					
+					for(int i = 0; i < doc.getElementsByTagName("sprite").getLength(); i++) {
+						Node s = doc.getElementsByTagName("sprite").item(i);
+					    NodeList d = s.getChildNodes();
+					    switch(d.item(1).getTextContent()) {
+					    case("player"): {
+					    	addToEnd(plays, i, null, null);
+					    	sprites[i] = new Sprite(i, d);
+					    	break;
+					    }
+	                    case("object"): {
+	                    	addToEnd(objs, i, null, null);
+	                    	sprites[i] = new Sprite(i, d);
+					    	break;
+					    }
+	                    case("character"): {
+	                    	addToEnd(chars, i, null, null);
+	                    	sprites[i] = new Sprite(i, d);
+					    	break;
+					    }			    
+					    }
+					}				
+					
+		}
+		public static Sprite nF(String name) {
+			for(int i = 0; i < sprites.length; i++) {
+				if(sprites[i].name == name) {
+					return(sprites[i]);
+				}
+			} 
+			return(null);			
+		}
+		public static Sprite idtF(int id, String type) {
+			switch(type) {
+			case("player"): {
+				return(sprites[plays[id]]);
+			}
+			case("character"): {
+				return(sprites[chars[id]]);			
+			}
+			case("object"): {
+				return(sprites[objs[id]]);				
+			}			
+			}
+			return(null);
+		}
+		public static Sprite idF(int id) {
+			return(sprites[id]);
+		}
+	}
 	
 	public static class Sprite {
 		int id, sid, sx, sy, px, py;
@@ -175,12 +164,42 @@ public class Gfx {
 			this.s = d.item(2).getTextContent();
 			this.sx = Integer.valueOf(d.item(3).getTextContent());
 			this.sy = Integer.valueOf(d.item(4).getTextContent());
-			this.px = Integer.valueOf(d.item(5).getTextContent());
-			this.py = Integer.valueOf(d.item(6).getTextContent());
+			if(this.type != "player") {
+				this.px = Integer.valueOf(d.item(5).getTextContent());
+				this.py = Integer.valueOf(d.item(6).getTextContent());
+			}
 			this.name = d.item(7).getTextContent();
 		}		
 
 	}
+	
+	
+	
+	
+	// RANDOM ACCESSORY FUNCTIONS
+	public static void addToEnd(int[] a, int v, String[] as, String vs) {
+		   if(a == null) {
+			   for(int i = 0; i < as.length; i++) {
+				   if(as[i] != null) {
+					   as[i] = vs;
+					   i = as.length;
+				   }			   
+			   }
+		   } else {
+			   for(int i = 0; i < a.length; i++) {
+				   if(a[i] == 0) {
+					   a[i] = v;
+					   i = a.length;
+				   }			   
+			   }
+		   }		
+		}
+	
+	public static void CLS(){   
+	    try {
+	        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+	    } catch (IOException | InterruptedException ex) {}
+	    }
 	
 }
 
